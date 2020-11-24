cd012264fff1d0905b0508bbf4c1f25508b45332
Raven <redstoyningbros@gmail.com>
2020-11-24 23:24:12 +0200

    Added Controller interface, abstract implementation and implementation for CityEntity

---
+++ src/main/java/ua/lviv/iot/hiberlab/controller/implementation/AbstractController.java
@@ -0,0 +1,115 @@
+package ua.lviv.iot.hiberlab.controller.implementation;
+
+import ua.lviv.iot.hiberlab.controller.Controller;
+import ua.lviv.iot.hiberlab.model.service.Service;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+
+public abstract class AbstractController<T> implements Controller<T> {
+
+    protected static final String ERROR_NO_MATCHES_FOUND = "[Error] No matches found";
+    protected static final String ERROR_INVALID_VALUE = "[Error] Entered invalid value";
+    protected static final String ERROR_MESSAGE_FORMAT = "[Error] Message: %s %n";
+
+    protected static final String ENTER_DATA_FORMAT = "Enter %s %s: %s %n";
+    protected static final String ENTER_COLUMN_NAME = "Choose column name to edit:";
+
+    private final Service<T> service;
+
+    public AbstractController(Service<T> service) {
+        this.service = service;
+    }
+
+    @Override
+    public List<T> findAll() {
+        return service.findAll();
+    }
+
+    @Override
+    public T findById(Integer id) {
+        return service.findById(id);
+    }
+
+    @Override
+    public T create(T entity) {
+        return service.create(entity);
+    }
+
+    @Override
+    public T update(Integer id, T entity) {
+        return service.update(id, entity);
+    }
+
+    @Override
+    public T delete(Integer id) {
+        return service.delete(id);
+    }
+
+    protected List<String> entityToList(T entity) {
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected <R> void enterValueForColumn(T entity, BiConsumer<T, R> setter, String columnName, Class<R> columnType, boolean optional, int maxLength) {
+        String limitation = "";
+        Scanner input = new Scanner(System.in, "UTF-8");
+        if (columnType == Integer.class) {
+            limitation = "(max value - " + Integer.MAX_VALUE + ")";
+        } else if (columnType == Date.class) {
+            limitation = "(format yyyy-mm-dd)";
+        } else if (columnType == BigDecimal.class) {
+            limitation = "(format xxx.xx)";
+        } else if (columnType == String.class) {
+            limitation = "(max length - " + maxLength + ")";
+        }
+        while (true) {
+            System.out.printf(ENTER_DATA_FORMAT, columnName, limitation, optional ? "(optional)" : "");
+            String inputText = input.nextLine();
+            try {
+                if (columnType == Integer.class) {
+                    Integer value = Integer.parseInt(inputText);
+                    setter.accept(entity, (R) value);
+                    break;
+                } else if (columnType == String.class) {
+                    if (inputText.equals("")) {
+                        if (optional) {
+                            setter.accept(entity, null);
+                            break;
+                        } else {
+                            System.out.println(ERROR_INVALID_VALUE);
+                        }
+                    } else {
+                        if (inputText.length() <= maxLength) {
+                            setter.accept(entity, (R) inputText);
+                            break;
+                        } else {
+                            System.out.println(ERROR_INVALID_VALUE);
+                        }
+                    }
+                } else if (columnType == Date.class) {
+                    Date date = Date.valueOf(inputText);
+                    setter.accept(entity, (R) date);
+                    break;
+                } else if (columnType == BigDecimal.class) {
+                    BigDecimal value = new BigDecimal(inputText);
+                    if (value.compareTo(BigDecimal.valueOf(0)) >= 0) {
+                        setter.accept(entity, (R) value);
+                        break;
+                    } else {
+                        System.out.println(ERROR_INVALID_VALUE);
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+                if (optional && inputText.equals("")) {
+                    break;
+                }
+                System.out.println(ERROR_INVALID_VALUE);
+                System.out.printf(ERROR_MESSAGE_FORMAT, e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/main/java/ua/lviv/iot/hiberlab/controller/implementation/CityController.java
@@ -0,0 +1,210 @@
+package ua.lviv.iot.hiberlab.controller.implementation;
+
+import ua.lviv.iot.hiberlab.model.entity.CityEntity;
+import ua.lviv.iot.hiberlab.model.entity.CountryEntity;
+import ua.lviv.iot.hiberlab.model.entity.RegionEntity;
+import ua.lviv.iot.hiberlab.model.service.Service;
+import ua.lviv.iot.hiberlab.model.service.implementation.CityService;
+import ua.lviv.iot.hiberlab.model.service.implementation.CountryService;
+import ua.lviv.iot.hiberlab.model.service.implementation.RegionService;
+import ua.lviv.iot.hiberlab.view.Formatter;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CityController extends AbstractController<CityEntity> {
+
+    private static final List<String> COLUMNS_NAMES = List.of("id", "name", "postal_code", "country_id", "region_id");
+
+    public CityController() {
+        super(new CityService());
+    }
+
+    @Override
+    public List<CityEntity> findAll() {
+        List<CityEntity> entities = super.findAll();
+        if (entities != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            for (CityEntity city : entities) {
+                body.add(entityToList(city));
+            }
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return entities;
+    }
+
+    @Override
+    public CityEntity findById(Integer id) {
+        CityEntity city = super.findById(id);
+        if (city != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            body.add(entityToList(city));
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return city;
+    }
+
+    @Override
+    public CityEntity create(CityEntity entity) {
+        super.enterValueForColumn(entity, CityEntity::setName, "name", String.class, false, 45);
+        super.enterValueForColumn(entity, CityEntity::setPostalCode, "postal_code", String.class, true, 6);
+        Scanner input = new Scanner(System.in, "UTF-8");
+        while (true) {
+            System.out.printf(ENTER_DATA_FORMAT, "country_id", "", "");
+            String inputText = input.nextLine();
+            try {
+                Integer value = Integer.parseInt(inputText);
+                Service<CountryEntity> countryService = new CountryService();
+                CountryEntity country = countryService.findById(value);
+                if (country != null) {
+                    entity.setCountryByCountryId(country);
+                    break;
+                } else {
+                    System.out.println(ERROR_INVALID_VALUE);
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_INVALID_VALUE);
+                System.out.printf(ERROR_MESSAGE_FORMAT, e.getMessage());
+            }
+        }
+        while (true) {
+            System.out.printf(ENTER_DATA_FORMAT, "region_id", "", "(optional)");
+            String inputText = input.nextLine();
+            try {
+                Integer value = Integer.parseInt(inputText);
+                Service<RegionEntity> regionService = new RegionService();
+                RegionEntity region = regionService.findById(value);
+                if (region != null) {
+                    entity.setRegionByRegionId(region);
+                    break;
+                } else {
+                    System.out.println(ERROR_INVALID_VALUE);
+                }
+            } catch (IllegalArgumentException e) {
+                if (!inputText.equals("")) {
+                    System.out.println(ERROR_INVALID_VALUE);
+                    System.out.printf(ERROR_MESSAGE_FORMAT, e.getMessage());
+                } else {
+                    break;
+                }
+            }
+        }
+        CityEntity createdEntity = super.create(entity);
+        List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+        List<List<String>> body = new LinkedList<>();
+        body.add(entityToList(createdEntity));
+        Formatter.formatTable(headerList, body);
+        return createdEntity;
+    }
+
+    @Override
+    public CityEntity update(Integer id, CityEntity entity) {
+//        CityEntity oldEntity = super.findById(id);
+        Scanner input = new Scanner(System.in, "UTF-8");
+        String column;
+        while (true) {
+            System.out.println(ENTER_COLUMN_NAME);
+            for (String columnName : COLUMNS_NAMES) {
+                System.out.println("\t-" + columnName);
+            }
+            String inputText = input.nextLine();
+            if (COLUMNS_NAMES.contains(inputText)) {
+                column = inputText;
+                break;
+            } else {
+                System.out.println(ERROR_INVALID_VALUE);
+            }
+        }
+        switch (column) {
+            case ("name"):
+                super.enterValueForColumn(entity, CityEntity::setName, "name", String.class, false, 45);
+                break;
+            case ("postal_code"):
+                super.enterValueForColumn(entity, CityEntity::setPostalCode, "postal_code", String.class, true, 6);
+                break;
+            case ("country_id"):
+                while (true) {
+                    System.out.printf(ENTER_DATA_FORMAT, "country_id", "", "");
+                    String inputText = input.nextLine();
+                    try {
+                        Integer value = Integer.parseInt(inputText);
+                        Service<CountryEntity> countryService = new CountryService();
+                        CountryEntity country = countryService.findById(value);
+                        if (country != null) {
+                            entity.setCountryByCountryId(country);
+                            break;
+                        } else {
+                            System.out.println(ERROR_INVALID_VALUE);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(ERROR_INVALID_VALUE);
+                        System.out.printf(ERROR_MESSAGE_FORMAT, e.getMessage());
+                    }
+                }
+                break;
+            case ("region_id"):
+                while (true) {
+                    System.out.printf(ENTER_DATA_FORMAT, "region_id", "", "(optional)");
+                    String inputText = input.nextLine();
+                    try {
+                        Integer value = Integer.parseInt(inputText);
+                        Service<RegionEntity> regionService = new RegionService();
+                        RegionEntity region = regionService.findById(value);
+                        if (region != null) {
+                            entity.setRegionByRegionId(region);
+                            break;
+                        } else {
+                            System.out.println(ERROR_INVALID_VALUE);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        if (!inputText.equals("")) {
+                            System.out.println(ERROR_INVALID_VALUE);
+                            System.out.printf(ERROR_MESSAGE_FORMAT, e.getMessage());
+                        } else {
+                            entity.setRegionByRegionId(null);
+                            break;
+                        }
+                    }
+                }
+                break;
+        }
+        CityEntity oldEntity = super.update(id, entity);
+        List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+        List<List<String>> body = new LinkedList<>();
+        body.add(entityToList(oldEntity));
+        body.add(entityToList(entity));
+        Formatter.formatTable(headerList, body);
+        return entity;
+    }
+
+    @Override
+    public CityEntity delete(Integer id) {
+        CityEntity city = super.delete(id);
+        if (city != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            body.add(entityToList(city));
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return city;
+    }
+
+    @Override
+    protected List<String> entityToList(CityEntity entity) {
+        String id = entity.getId().toString();
+        String name = entity.getName();
+        String postalCode = entity.getPostalCode() == null ? "-" : entity.getPostalCode();
+        String countryId = entity.getCountryByCountryId().getId().toString();
+        String regionId = entity.getRegionByRegionId() == null ? "-" : entity.getRegionByRegionId().getId().toString();
+        return new LinkedList<>(List.of(id, name, postalCode, countryId, regionId));
+    }
+}
