53554326d19b3d1b6ab017bd94d1fedbdffc5cfb
Raven <redstoyningbros@gmail.com>
2020-11-25 00:35:01 +0200

    Added methods for PostController

--- src/main/java/ua/lviv/iot/hiberlab/controller/implementation/PostController.java
+++ src/main/java/ua/lviv/iot/hiberlab/controller/implementation/PostController.java
@@ -1,11 +1,109 @@
 package ua.lviv.iot.hiberlab.controller.implementation;
 
 import ua.lviv.iot.hiberlab.model.entity.PostEntity;
-import ua.lviv.iot.hiberlab.model.service.Service;
 import ua.lviv.iot.hiberlab.model.service.implementation.PostService;
+import ua.lviv.iot.hiberlab.view.Formatter;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class PostController extends AbstractController<PostEntity> {
+
+    private static final List<String> COLUMNS_NAMES = List.of("id", "post");
+
     public PostController() {
         super(new PostService());
+    }
+
+    @Override
+    public List<PostEntity> findAll() {
+        List<PostEntity> entities = super.findAll();
+        if (entities != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            for (PostEntity entity : entities) {
+                body.add(entityToList(entity));
+            }
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return entities;
+    }
+
+    @Override
+    public PostEntity findById(Integer id) {
+        PostEntity entity = super.findById(id);
+        if (entity != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            body.add(entityToList(entity));
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return entity;
+    }
+
+    @Override
+    public PostEntity create(PostEntity entity) {
+        super.enterValueForColumn(entity, PostEntity::setPost, "post", String.class, false, 45);
+        PostEntity createdEntity = super.create(entity);
+        List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+        List<List<String>> body = new LinkedList<>();
+        body.add(entityToList(createdEntity));
+        Formatter.formatTable(headerList, body);
+        return createdEntity;
+    }
+
+    @Override
+    public PostEntity update(Integer id, PostEntity entity) {
+        Scanner input = new Scanner(System.in, "UTF-8");
+        String column;
+        while (true) {
+            System.out.println(ENTER_COLUMN_NAME);
+            for (String columnName : COLUMNS_NAMES) {
+                System.out.println("\t-" + columnName);
+            }
+            String inputText = input.nextLine();
+            if (COLUMNS_NAMES.contains(inputText)) {
+                column = inputText;
+                break;
+            } else {
+                System.out.println(ERROR_INVALID_VALUE);
+            }
+        }
+        if ("post".equals(column)) {
+            super.enterValueForColumn(entity, PostEntity::setPost, "post", String.class, false, 45);
+        }
+        PostEntity oldEntity = super.update(id, entity);
+        List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+        List<List<String>> body = new LinkedList<>();
+        body.add(entityToList(oldEntity));
+        body.add(entityToList(entity));
+        Formatter.formatTable(headerList, body);
+        return entity;
+    }
+
+    @Override
+    public PostEntity delete(Integer id) {
+        PostEntity entity = super.delete(id);
+        if (entity != null) {
+            List<String> headerList = new LinkedList<>(COLUMNS_NAMES);
+            List<List<String>> body = new LinkedList<>();
+            body.add(entityToList(entity));
+            Formatter.formatTable(headerList, body);
+        } else {
+            System.out.println(ERROR_NO_MATCHES_FOUND);
+        }
+        return entity;
+    }
+
+    @Override
+    protected List<String> entityToList(PostEntity entity) {
+        String id = entity.getId().toString();
+        String post = entity.getPost();
+        return new LinkedList<>(List.of(id, post));
     }
 }
